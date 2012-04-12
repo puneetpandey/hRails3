93d97bf18a67f01944f3f10243a942ed56678dda
unknown <puneet.pandey@.synapse.com>
2012-04-12 13:01:32 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HiloRails3::Application.config.secret_token = '1cad6dbc98358bde49477c4fdfec45e91bfbd7f08b8249c0f9a709b53b182e9d2563aa92fa196ae931f282834f2b7f2fd5892c4226647174ab2f385346012689'
